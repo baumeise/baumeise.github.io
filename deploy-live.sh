a11c5e7c04f29db59014e069bb8eed0521ba9f3a
moritzgvt <hello@moritzgut.de>
2019-11-14 09:42:50 +0100

    Adjust deployment scripts

--- deploy-live.sh
+++ deploy-live.sh
@@ -3,7 +3,7 @@
 # abort on errors
 set -e
 
-sed -i -n "s/\(base:\).*/\1 \'\/amsel\/\'/" ./public/.vuepress/config.js
+sed -i "s/\(base:\).*/\1 \'\/amsel\/\'/" ./public/.vuepress/config.js
 
 # build
 vuepress build public
@@ -19,4 +19,4 @@ git push -f git@github.com:moritzgvt/amsel.git master:gh-pages
 
 cd -
 
-sed -i -n "s/\(base:\).*/\1 \'\/amseldocs\/\'/" ./public/.vuepress/config.js
+sed -i "s/\(base:\).*/\1 \'\/amseldocs\/\'/" ./public/.vuepress/config.js
