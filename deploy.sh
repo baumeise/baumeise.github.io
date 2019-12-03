4ca5785d84a4b1d69ce8e9d44e1b9c386ef97480
moritzgvt <hello@moritzgut.de>
2019-12-03 16:24:31 +0100

    Update root path

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,8 @@
 # abort on errors
 set -e
 
+sed -i -n "s/\(base:\).*/\1 \'\/amsel\/\'\,/" public/.vuepress/config.js
+
 # build
 vuepress build public
 
@@ -19,3 +21,4 @@ git commit -m "$any"
 git push -f git@github.com:baumeise/amsel master:gh-pages
 
 cd -
+sed -i -n "s/\(base:\).*/\1 \'\/\'\,/" public/.vuepress/config.js
