cb5ed0b017e8431ad97dc933c061b5120030ef79
moritzgvt <hello@moritzgut.de>
2019-12-03 12:28:08 +0100

    Fix deployment

--- deploy.sh
+++ deploy.sh
@@ -16,6 +16,6 @@ printf "Enter commit message: "
 read any
 git commit -m "$any"
 
-git push -f git@github.com:baumeise/baumeise.github.io.git master:amsel
+git push -f git@github.com:baumeise/baumeise.github.io.git page:master
 
 cd -
